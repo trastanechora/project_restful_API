4e07f4b193b6538a9beccfd2d08416fc39c2d948
trastanechora <trastanechora@gmail.com>
2019-02-22 02:37:35 +0700

    core algorighm combining music genre and weather

--- blueprints/core/__init__.py
+++ blueprints/core/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask_restful import Api, Resource, reqparse, marshal
 from .. import *
 from ..song import Songs
+import random
 
 import requests
 
@@ -16,61 +17,15 @@ class CoreResources(Resource):
     wio_apikey = '001de4440e814c16bc45197fd601ef9d'
 
     category_1 = [
-       "seen live",
-       "female vocalists",
-       "jazz",
-       "classic rock",
-       "ambient",
-       "80s",
-       "chillout",
-       "acoustic",
+       "seen live", "female vocalists", "jazz", "classic rock", "ambient", "80s", "chillout", "acoustic",
     ]
 
     category_2 = [
-        "singer-songwriter",
-        "british", 
-        "soul",
-        "blues",
+        "singer-songwriter", "british", "soul", "blues",
     ]
 
     category_3 = [
-        "rock",
-        "electronic",
-        "alternative", 
-        "indie", 
-        "pop",
-        "metal", 
-        "alternative rock",
-        "experimental", 
-        "folk", 
-        "punk", 
-        "indie rock", 
-        "hard rock", 
-        "Hip-Hop", 
-        "instrumental",
-        "black metal",
-        "dance",
-        "Progressive rock", 
-        "death metal", 
-        "heavy metal", 
-        "hardcore",
-        "electronica",
-        "Classical",
-        "industrial", 
-        "Soundtrack", 
-        "rap",
-        "punk rock", 
-        "thrash metal", 
-        "90s",
-        "metalcore", 
-        "psychedelic",
-        "post-rock", 
-        "Progressive metal", 
-        "german", 
-        "funk", 
-        "hip hop", 
-        "new wave", 
-        "trance"
+        "rock", "electronic", "alternative", "indie", "pop", "metal", "alternative rock", "experimental", "folk", "punk", "indie rock", "hard rock", "Hip-Hop", "instrumental", "black metal", "dance", "Progressive rock", "death metal", "heavy metal", "hardcore", "electronica", "Classical", "industrial", "Soundtrack", "rap", "punk rock", "thrash metal", "90s", "metalcore", "psychedelic", "post-rock", "Progressive metal", "german", "funk", "hip hop", "new wave", "trance"
     ]
 
     def get(self):
@@ -88,39 +43,45 @@ class CoreResources(Resource):
 
         weather_code = int(current['data'][0]['weather']['code'])
 
-
-
-        # return {
-        #     'country_code': current['data'][0]['country_code'],
-        #     'city_name': current['data'][0]['city_name'],
-        #     'weather_code': current['data'][0]['weather']['code'],
-        #     'weather_desc': current['data'][0]['weather']['description'],
-        #     'temp': current['data'][0]['temp']
-        # }
-        # return int(current['data'][0]['weather']['code'])
-
         parser = reqparse.RequestParser()
         parser.add_argument('p', type=int, location='args', default=1)
         parser.add_argument('rp', type=int, location='args', default=5)
-        # parser.add_argument('name', type=str, location='args')
         args = parser.parse_args()
 
         offset = (args['p'] * args['rp']) - args['rp']
 
         qry = Songs.query
 
-        # if args['name'] is not None:
-        #     qry = qry.filter(Books.book_title.like("%"+args['name']+"%"))
-        
-        qry = qry.filter_by(genre = "pop")
+        result = []
         rows = []
-        # print(args)
-        # print(offset)
-        for row in qry.all():
-            # if row.book_status == "Show":
-            #     rows.append(marshal(row, Books.response_field))
+        for row in qry.limit(args['rp']).offset(offset).all():
             rows.append(marshal(row, Songs.response_fields))
+
+        if weather_code <= 600:
+            test2 = []
+            for genre in self.category_1:
+                test = qry.filter_by(genre = genre)
+                for row in test.limit(args['rp']).offset(offset).all():
+                    test2.append(marshal(row, Songs.response_fields))
+        elif weather_code > 600 and weather_code < 800:
+            test2 = []
+            for genre in self.category_2:
+                test = qry.filter_by(genre = genre)
+                for row in test.limit(args['rp']).offset(offset).all():
+                    test2.append(marshal(row, Songs.response_fields))
+        else:
+            test2 = []
+            for genre in self.category_3:
+                test = qry.filter_by(genre = genre)
+                for row in test.limit(args['rp']).offset(offset).all():
+                    test2.append(marshal(row, Songs.response_fields))
+
+        output = []
+        while len(output) < 10:
+            temp = random.choice(test2)
+            if temp not in output:
+                output.append(temp)
         
-        return rows, 200, { "content-type": "application/json" }
+        return output, 200, { "content-type": "application/json" }
 
 api.add_resource(CoreResources, '')
